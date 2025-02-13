e5c2e1bc853ce068731498e74452ed9bbd6a73a2
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-02-13 17:10:18 +0530

    Basic setup for web pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom"
+import { Navbar } from "./components/component_index.js"
 
 function App() {
 
   return (
     <>
       <ToastContainer position='bottom-right'/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
