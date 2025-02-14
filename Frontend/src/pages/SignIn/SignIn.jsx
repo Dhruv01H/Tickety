7898e233ea3e406b358148c4d0f49de6687c87e7
Dhruv01H <hirenmehtadhruv@gmail.com>
2025-02-14 23:57:03 +0530

    Data from frontend to backend in sign in

--- Frontend/src/pages/SignIn/SignIn.jsx
+++ Frontend/src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,39 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+//import cors
+
+
 
 function SignIn() {
+
+  
   const navigate = useNavigate();
-  const handleForm = (e) => {
+  const [error, setError] = useState("");
+
+
+  const handleForm = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const obj = Object.fromEntries(formData.entries());
+    console.log("Sending data to backend:", obj); 
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/auth/signin',obj ,{
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      alert("Successful Login");
+      
+      navigate("/");
+      
+    } catch (error) {
+      console.error("Error:", error.response?.data || error.message);
+      setError("");
+    }
+
     console.log(obj);
   };
 
@@ -16,12 +44,12 @@ function SignIn() {
           <form action="" onSubmit={handleForm} className="flex flex-col gap-4 mb-8">
             <h1 className="mb-4 text-3xl font-bold">Sign In</h1>
             <div className="flex flex-col items-start gap-2 mb-1">
-              <label htmlFor="Email" className="text-lg">
+              <label htmlFor="Email" className="text-lg" >
                 E-mail
               </label>
               <input
                 type="email"
-                name="Email"
+                name="email"
                 placeholder="Email"
                 className="w-full px-3 py-1 text-xl rounded-lg text-frost outline-0 bg-slate-400"
                 required
@@ -34,7 +62,7 @@ function SignIn() {
               </label>
               <input
                 type="password"
-                name="Password"
+                name="password"
                 placeholder="Password"
                 required
                 minLength={8}
