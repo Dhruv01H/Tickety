513d98d75d86050bc7bedd4bddb7085aafcec4c2
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-02-17 09:29:26 +0530

    Create a contact page and home carousal

--- Frontend/src/pages/page_index.js
+++ Frontend/src/pages/page_index.js
@@ -2,10 +2,12 @@ import Home from "./Home/Home";
 import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
 import Profile from "./Profile/Profile";
+import Contact from "./Contact/Contact";
 
 export { 
     Home,
     SignIn,
     SignUp,
     Profile,
+    Contact,
 }
