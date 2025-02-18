24bec73c4eb2ae62680ed5fb1bb56c69c6b09ae4
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-02-18 20:23:25 +0530

    Create transaprent navbar UI

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -4,61 +4,66 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper/modules";
 import { assets } from "../../assets/assets";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-      {/* ========= Home Page ============= 
-      <div>
-        <img
-          src={assets.header1}
-          alt=""
-          className="absolute -z-10 top-0 w-full min-h-screen aspect-[4/3]"
-        />
-      </div> */}
-
-      {/* Hero Section with auto carousal */}
-      <div className="-z-10">
+      <div className="relative w-full h-[80vh] overflow-hidden">
         <Swiper
-          spaceBetween={5}
+          spaceBetween={0}
           centeredSlides={true}
           autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
+            delay: 2000,
+            disableOnInteraction: true,
+          }}
+          pagination={{
+            clickable: true,
           }}
           modules={[Autoplay, Pagination]}
-          className="mySwiper"
+          className="absolute top-0 left-0 z-0 w-full h-[80vh] mySwiper"
         >
-        
-        <SwiperSlide>
-              <div className="">
-                <img
-                  src={assets.header1}
-                  alt="Hero 1"
-                  className="w-full h-full aspect-[9/16] sm:aspect-[9/10] md:aspect-[7/5] lg:aspect-[7/3] xl:aspect-[7/4]"
-                />
-              </div>
-            </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={assets.header1}
+              alt="Hero 1"
+              className="object-cover w-full h-[80vh]"
+            />
+          </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="">
-                <img
-                  src={assets.header2}
-                  alt="Hero 2"
-                  className="w-full h-full aspect-[9/16] sm:aspect-[9/10] md:aspect-[7/5] lg:aspect-[7/3] xl:aspect-[7/4]"
-                />
-              </div>
-            </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={assets.header2}
+              alt="Hero 2"
+              className="object-cover w-full h-[80vh]"
+            />
+          </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="">
-                <img
-                  src={assets.header3}
-                  alt="Hero 3"
-                  className="w-full h-full aspect-[9/16] sm:aspect-[9/10] md:aspect-[7/5] lg:aspect-[7/3] xl:aspect-[7/4]"
-                />
-              </div>
-            </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={assets.header3}
+              alt="Hero 3"
+              className="object-cover w-full h-[80vh]"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img
+              src={assets.header4}
+              alt="Hero 4"
+              className="object-cover w-full h-[80vh]"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="">
+              <img
+                src={assets.header5}
+                alt="Hero 5"
+                className="object-cover w-full h-[80vh]"
+              />
+            </div>
+          </SwiperSlide>
         </Swiper>
       </div>
     </>
